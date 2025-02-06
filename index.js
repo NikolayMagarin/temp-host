07fe4b51f34db4fe205ca54e5a70f4bb3446313e
Nikolay Magarin <magarinnikolay@gmail.com>
2025-02-06 21:50:11 +0300

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 8080;
+
+const mainHtmlPath = path.join(__dirname, 'public', 'index.html');
+const notFoundHtmlPath = path.join(__dirname, 'public', 'notFound.html');
+
+app.get('/', (req, res) => {
+  res.sendFile(mainHtmlPath);
+});
+
+app.get('/:page', (req, res) => {
+  const htmlPath = path.join(
+    __dirname,
+    'public',
+    req.params.page,
+    'index.html'
+  );
+
+  if (fs.existsSync(htmlPath)) {
+    res.sendFile(htmlPath);
+  } else {
+    res.sendFile(notFoundHtmlPath);
+  }
+});
+
+app.get('/:page/images/:image', (req, res) => {
+  const imagePath = path.join(
+    __dirname,
+    'public',
+    req.params.page,
+    'images',
+    req.params.image
+  );
+
+  if (fs.existsSync(imagePath)) {
+    res.sendFile(imagePath);
+  } else {
+    res.end();
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(notFoundHtmlPath);
+});
+
+app.listen(PORT);
